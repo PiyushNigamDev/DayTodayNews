05d59b1bd759c748c949685bcdb839fcd369e4f8
Piyush Nigam <piyushnigam2355@gmail.com>
2026-03-28 18:37:50 +0530

    Fix auth file casing issue

--- Backend/router/Router.js
+++ Backend/router/Router.js
@@ -1,17 +1,19 @@
 import express from "express"
 
  import * as studentRoutes from "../Controller/student.controller.js"
-import upload from "../middleware/multer.js"
-import  Auth from "../middleware/auth.js"; 
+import upload from "../middleware/multer.js";
+import auth from "../middleware/Auth.js";
+
+  
 const router=express.Router()
 
  
 router.post("/",studentRoutes.createStudent)
 router.get("/gets",studentRoutes.getUser)
-router.put("/:id",Auth,upload.single("thumbnail"), studentRoutes.updateNews)
+router.put("/:id",auth,upload.single("thumbnail"), studentRoutes.updateNews)
 router.get("/:id",studentRoutes.getbyId)
-router.delete("/:id",Auth, studentRoutes.delUser)
-router.post("/upload",Auth,upload.single("thumbnail"),studentRoutes.uploadNews)
+router.delete("/:id",auth, studentRoutes.delUser)
+router.post("/upload",auth,upload.single("thumbnail"),studentRoutes.uploadNews)
 
 router.post("/register", studentRoutes.registerStudent)
 router.post("/login",studentRoutes.loginStudent)
