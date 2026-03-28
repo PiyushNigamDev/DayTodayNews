d5e070438fa6f9a71d98a2cb3b0bd5968de88e8b
Piyush Nigam <piyushnigam2355@gmail.com>
2026-03-28 15:32:18 +0530

    Updated backend with Atlas connection

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 dotenv.config()
 const connectDB=async ()=>{
     try{
-        await mongoose.connect(process.env.MONO_DB_URL);
+     await  mongoose.connect(process.env.MONO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
            console.log("Database connected successfully")
         } catch(err){
         console.log(err.message)
