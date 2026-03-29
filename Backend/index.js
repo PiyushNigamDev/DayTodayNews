47542afc92b21118348af7d066b1448dd912c636
Piyush Nigam <piyushnigam2355@gmail.com>
2026-03-29 14:35:24 +0530

    Fix CORS issue

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,12 @@ dotenv.config();
 connectDB();
 const app=express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://day-today-news.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json())
 // const port=3000
 
